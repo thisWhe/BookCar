fce427b93cb70c450b810fd61102be2f59a683c1
Emir <87826166+thisWhe@users.noreply.github.com>
2024-01-12 12:02:42 +0300

    Repository.cs

--- Solution1/Infrastructure/UdemyCarBook.Persistence/Repositories/Repository.cs
+++ Solution1/Infrastructure/UdemyCarBook.Persistence/Repositories/Repository.cs
@@ -12,44 +12,44 @@ namespace UdemyCarBook.Persistence.Repositories
     public class Repository<T> : IRepository<T> where T : class
     {
 
-        private readonly CarBookContext _context; // CarBookContext sınıfından bi tane _context fieldi ornekledik
+        private readonly CarBookContext _context; 
 
-        public Repository(CarBookContext context) //  dependency injection uygulayabilmek icin class yazısının üstüne gelip ctrl + . yapıp generate constructor... yazısına basıp ok diyip yapıcı metot olarak buraya gelmis oldu
+        public Repository(CarBookContext context)
         {
-            _context = context; // bunun üzerinden diğer metotlarımın içerisini dolduruyo olucaz
+            _context = context; 
         }
 
-        public async Task CreateAsync(T entity)   // bunları iplement edim metotların icini doldurmak icin hazır hale geldi
+        public async Task CreateAsync(T entity)   
 
         {
-            // T ye göre ayarla ondan sonra add ve entityden gelen değer
-            _context.Set<T>().Add(entity);  // bu metot şu an kaydetme işlemini gerceklestiriyo olucak
+          
+            _context.Set<T>().Add(entity);  
           await  _context.SaveChangesAsync();
         }
 
 
         public async Task<List<T>> GettAllAsync()
         {
-            return await _context.Set<T>().ToListAsync(); // geriye bütün listeyi döndür 
+            return await _context.Set<T>().ToListAsync(); 
         }
 
         public async Task<T> GetByIdAsync(int id)
         {
-            return await _context.Set<T>().FindAsync(id); // idye göre bul
+            return await _context.Set<T>().FindAsync(id); 
         }
 
         public async Task RemoveAsync(T entity)
         {
-            _context.Set<T>().Remove(entity); // entityden gelen değeri kaldır
+            _context.Set<T>().Remove(entity); 
             await _context.SaveChangesAsync();
         }
 
         public async Task UpdateAsync(T entity)
         {
-           _context.Set<T>().Update(entity); // entityden gelen değer
+           _context.Set<T>().Update(entity); 
             await _context.SaveChangesAsync();
         }
 
-        // repository sınıfımız hazır hale geldi
+        
     }
 }
