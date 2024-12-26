90676376e1da1f830e288c3a9f3e586c9b98e892
Emir <87826166+thisWhe@users.noreply.github.com>
2024-12-26 13:04:01 +0300

    IRepository.cs

--- Solution1/Core/UdemyCarBook.Application/Interfaces/IRepository.cs
+++ Solution1/Core/UdemyCarBook.Application/Interfaces/IRepository.cs
@@ -8,18 +8,18 @@ namespace UdemyCarBook.Application.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        //listeleme işlemi için;
+       
         Task<List<T>> GettAllAsync();
 
-        Task<T> GetByIdAsync(int id); // Id'ye gore getirme islemi için
+        Task<T> GetByIdAsync(int id); 
       
-        Task CreateAsync(T entity); // daha sonra listeleme islemi icin. / T türünde bir entity alıcak
+        Task CreateAsync(T entity); 
 
         Task UpdateAsync(T entity);
 
-        Task RemoveAsync(T entity); // T tipinde entity gonderiyoruz
+        Task RemoveAsync(T entity); 
        
 
-        // IRepository Interfacei olusturduk burası bizim bütün entitylerimizin temel olarak kullanıcagı 5 temel metot
+        
     }
 }
