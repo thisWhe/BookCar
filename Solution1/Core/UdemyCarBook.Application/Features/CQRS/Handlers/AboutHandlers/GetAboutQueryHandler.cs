2c9a5109b5910ed181abb9210e258004293198bd
Emir <87826166+thisWhe@users.noreply.github.com>
2024-12-26 13:01:52 +0300

    GetAboutQueryHandler.cs

--- Solution1/Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
+++ Solution1/Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -13,17 +13,17 @@ namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
     {
         private readonly IRepository<About> _repository;
 
-        public GetAboutQueryHandler(IRepository<About> repository) // Listenin içinde GettAllAsync var bunun verileri de Aboutun icinden gelicek
+        public GetAboutQueryHandler(IRepository<About> repository) 
         {
             _repository = repository;
         }
 
         public async Task<List<GetAboutQueryResult>> Handle()
         {
-            var values = await _repository.GettAllAsync(); // bu bize sorunsuz sekilde tüm verileri listeliceki
+            var values = await _repository.GettAllAsync(); /
             return values.Select(x => new GetAboutQueryResult
-            {/// AboutID ye x'den gelen değer atansın / x ise listemizden gelen verileri tutucak / Listenin içinde GettAllAsync var bunun verileri de Aboutun icinden gelicek
-                AboutID = x.AboutID,// parantezler icine ctrl bosluk yapıyoruz bize gelen propları goruyoruz  GetAboutQueryResult'daki proplar
+            {
+                AboutID = x.AboutID,
                 Description = x.Description,
                 Title = x.Title,
                 ImageUrl = x.ImageUrl,               
